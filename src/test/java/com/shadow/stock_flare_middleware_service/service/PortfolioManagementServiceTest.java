4a47aa1adfd5ff9a3ff11532f9383c9a2fa4e9da
Javed Wright <jdwright870@gmail.com>
2024-03-29 19:12:08 -0500

    add tests for trade ceation

--- src/test/java/com/shadow/stock_flare_middleware_service/service/PortfolioManagementServiceTest.java
+++ src/test/java/com/shadow/stock_flare_middleware_service/service/PortfolioManagementServiceTest.java
@@ -1,10 +1,13 @@
 package com.shadow.stock_flare_middleware_service.service;
 
 import com.shadow.stock_flare_middleware_service.constants.PortfolioType;
+import com.shadow.stock_flare_middleware_service.constants.TradeType;
 import com.shadow.stock_flare_middleware_service.exception.ResourceNotFoundException;
 import com.shadow.stock_flare_middleware_service.repository.PortfolioRepository;
+import com.shadow.stock_flare_middleware_service.repository.PortfolioTradeRepository;
 import com.shadow.stock_flare_middleware_service.repository.UserRepository;
 import com.shadow.stock_flare_middleware_service.repository.entity.Portfolio;
+import com.shadow.stock_flare_middleware_service.repository.entity.PortfolioTrade;
 import org.hibernate.JDBCException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.swing.text.html.Option;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,6 +35,9 @@ class PortfolioManagementServiceTest {
 
     @Mock
     PortfolioRepository portfolioRepositoryMock;
+
+    @Mock
+    PortfolioTradeRepository portfolioTradeRepositoryMock;
 
     @InjectMocks
     PortfolioManagementService portfolioManagementService;
@@ -74,5 +82,55 @@ class PortfolioManagementServiceTest {
         Optional<Portfolio> portfolioOpt = portfolioManagementService.createPortfolio("1", "Nickname", "912397123", PortfolioType.STOCK.name());
 
         assertEquals(expectedPortfolio, portfolioOpt.get());
+    }
+
+    @Test
+    @DisplayName("Throw exception while creating portfolio trade given unknown portfolio ID")
+    public void testCreateTradeGivenTradeDetailsWhenPortfolioIDDosNotExistThenThrowResourceNotFoundException() {
+        when(portfolioRepositoryMock.existsById(any())).thenReturn(false);
+
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> {
+            portfolioManagementService.createTrade(null, null, null, null, null, null, null, null, null);
+        });
+
+        assertEquals("Unknown portfolio ID", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Return null on error while saving portfolio trade details")
+    public void testCreateTradeGivenTradeDetailsWhenExceptionIsThrownWhileSavingTradeThenReturnEmpty() {
+        when(portfolioRepositoryMock.existsById(any())).thenReturn(true);
+
+        when(portfolioTradeRepositoryMock.save(any())).thenThrow(JDBCException.class);
+
+        PortfolioTrade portfolioTrade = portfolioManagementService.createTrade(null, null, null, null, null, null, null, null, null);
+
+        assertNull(portfolioTrade);
+
+    }
+
+    @Test
+    @DisplayName("Return portfolio Trade details when saved")
+    public void testCreateTradeGivenTradeDetailsWhenTradeIsSavedThenReturnTradeDetailsDetails() {
+
+        PortfolioTrade expectedTrade = new PortfolioTrade();
+        expectedTrade.setPortfolioId("12345");
+        expectedTrade.setSymbol("SYM");
+        expectedTrade.setNoOfUnits(new BigDecimal(1));
+        expectedTrade.setAmountPerUnit(new BigDecimal(1));
+        expectedTrade.setType(TradeType.BUY.name());
+        expectedTrade.setTransactionDate(LocalDate.now());
+        expectedTrade.setTaxes(new BigDecimal(0));
+        expectedTrade.setBrokerFees(new BigDecimal(0));
+        expectedTrade.setOtherFees(new BigDecimal(0));
+
+        when(portfolioRepositoryMock.existsById(any())).thenReturn(true);
+        when(portfolioTradeRepositoryMock.save(any())).thenReturn(expectedTrade);
+
+        PortfolioTrade portfolioTrade = portfolioManagementService.createTrade("12345", "SYM",
+                new BigDecimal(1), new BigDecimal(1), TradeType.BUY.name(), expectedTrade.getTransactionDate(),
+                null, null, null);
+
+        assertEquals(expectedTrade, portfolioTrade);
     }
 }
